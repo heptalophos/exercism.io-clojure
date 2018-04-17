2f4c1469818d0371aa36e90436502a300ac6f836
Chris NP <zerodeltahedge@gmail.com>
2018-04-17 07:09:39 +0300

    new problem (anagram)

--- anagram/src/anagram.clj
+++ anagram/src/anagram.clj
@@ -1,5 +1,36 @@
-(ns anagram)
+(ns anagram 
+  (:require [clojure.string :refer [join lower-case upper-case]]))
 
-(defn anagrams-for [] ;; <- arglist goes here
-  ;; your code goes here
+(defn string-sort 
+  "Sort a string"
+  [w]
+  (join "" (map str (sort (seq (lower-case w)))))
 )
+
+(defn anagram? 
+  "compute if candidate is anagram of word"
+  [word cand]
+  (= word (string-sort cand))
+)
+
+(defn not-same? 
+  "compute if candidate is not the same as the word"
+  [word cand]
+  (not= word cand)  
+)  
+
+(defn not-capital?
+  "candidate is not a capital word"
+  [word cand]
+  (not= word (upper-case cand))  
+)  
+
+(defn anagrams-for
+  "words list from words that are anagram with anagram"
+  [anagram word-list]
+  (let [sorted-anagram (string-sort anagram)]
+  (filter 
+    #(and (anagram? sorted-anagram %)
+          (not-same? anagram %)
+          (not-capital? anagram %))
+  word-list) ))
