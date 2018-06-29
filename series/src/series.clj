a9fdd6ff00ea31bd57f65f301eea4ba29a5ac389
Zero Delta <zerodeltahedge@gmail.com>
2018-06-29 16:40:01 +0300

    series

--- series/src/series.clj
+++ series/src/series.clj
@@ -1,3 +1,9 @@
 (ns series)
 
-(defn slices [string n])
+(defn slices [string n]
+  (if (zero? n)
+    [""]
+    (loop [string string, acc []]
+      (if (< (count string) n)
+        acc
+        (recur (rest string) (conj acc (apply str (take n string))))))))
