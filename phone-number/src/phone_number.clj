a540d4a8c0d9e27a1893a730d367a3e5b6d54560
Zero Delta <zerodeltahedge@gmail.com>
2018-07-03 00:05:41 +0300

    phone-number

---
+++ phone-number/src/phone_number.clj
@@ -0,0 +1,17 @@
+(ns phone-number
+  (:require [clojure.string :as str]))
+
+(defn number [num-string] 
+  (let [ph (str/replace num-string #"\D" ""  )
+      length (count ph)]
+    (cond 
+      (and (= 11 length) (= \1 (first ph))) (subs ph 1)
+      (= 10 length) ph 
+      :else "0000000000" )))
+
+(defn area-code [num-string] 
+  (subs (number num-string) 0 3))
+
+(defn pretty-print [num-string] 
+  (let [number (number num-string)]
+    (format "(%s) %s-%s" (subs number 0 3) (subs number 3 6) (subs number 6))))
