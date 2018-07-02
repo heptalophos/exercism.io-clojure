f9e3a7aab4f3b4aa8a0bce0fe51af4dcb6108c5e
Zero Delta <zerodeltahedge@gmail.com>
2018-07-03 01:04:14 +0300

    grade-school

---
+++ grade-school/src/grade_school.clj
@@ -0,0 +1,17 @@
+(ns grade-school)
+
+(defn grade 
+    [school grade]  
+    (get school grade []))
+
+(defn add 
+    [school name in-grade]
+    (merge school 
+           {in-grade (conj (grade school in-grade) name)}))
+
+(defn sorted 
+    [school] 
+    (into (sorted-map)
+      (map (fn [[grade names]]
+             [grade (sort names)]) school)))
+
