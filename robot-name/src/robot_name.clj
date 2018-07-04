ef105c1b0a8de8f5e4b5d855dd096c02a8c9a8dd
Zero Delta <zerodeltahedge@gmail.com>
2018-07-04 15:54:59 +0300

    robot-name

---
+++ robot-name/src/robot_name.clj
@@ -0,0 +1,20 @@
+(ns robot-name)
+
+(defn generate-name 
+  []
+  (let [ALPHABET (range 65 91)]
+    (format "%1$s%2$s%3$03d" (char (rand-nth ALPHABET)) (char (rand-nth ALPHABET)) (rand-int 1000))))
+
+(defrecord Robot [name])
+
+(defn robot 
+  []
+  (Robot. (atom (generate-name))))
+
+(defn robot-name 
+  [robot]
+  @(:name robot))
+
+(defn reset-name 
+  [robot]
+  (reset! (:name robot) (generate-name))) 
