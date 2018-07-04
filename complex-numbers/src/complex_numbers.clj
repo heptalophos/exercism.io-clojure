37b7b458539a1690a3b0347a50d6b9d36de1c6fb
Zero Delta <zerodeltahedge@gmail.com>
2018-07-04 14:18:18 +0300

    complex-numbers

---
+++ complex-numbers/src/complex_numbers.clj
@@ -0,0 +1,41 @@
+(ns complex-numbers)
+
+(defn real 
+  [[a b]]
+  a)
+
+(defn imaginary 
+  [[a b]]
+  b)
+
+(defn abs 
+  [[a b]]
+  (Math/sqrt (apply + (map #(Math/pow % 2) [a b]))))
+
+(defn conjugate 
+  [[a b]]
+  [a (* b -1)])
+
+(defn add 
+  [[a b] [c d]]
+  [(+ a c) (+ b d)] )
+
+(defn sub [[a b] [c d]] 
+  [(- a c) (- b d)] )
+
+(defn mul 
+  [[a b] [c d]] 
+  [(- (* a c) (* b d))
+  (+ (* a d) (* b c))])
+
+(defn- denom 
+  [c n]
+  (let [absc (abs c)]
+    (/ n (* absc absc))))
+
+
+(defn div [[a b] [c d]] 
+  (->> (conjugate [c d])
+       (mul [a b])
+       (map (partial denom [c d]))
+       (apply vector)))
