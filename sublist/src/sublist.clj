95a80f49753ed79075ba5bc6730975235e66c009
Zero Delta <zerodeltahedge@gmail.com>
2018-07-07 16:02:29 +0300

    sublist

---
+++ sublist/src/sublist.clj
@@ -0,0 +1,12 @@
+(ns sublist
+  (:require [clojure.string :refer [join includes?]]))
+
+(defn classify 
+  [list1 list2] 
+  (let [s1 (join "," list1)
+        s2 (join "," list2)]
+  (cond 
+    (= s1 s2) :equal 
+    (includes? s1 s2) :superlist
+    (includes? s2 s1) :sublist
+    :else :unequal)))
