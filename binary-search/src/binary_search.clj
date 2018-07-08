50beb4a3b99f1ca54b3c643e6d340cc5f59a1f41
Zero Delta <zerodeltahedge@gmail.com>
2018-07-08 16:59:35 +0300

    binary-search

---
+++ binary-search/src/binary_search.clj
@@ -0,0 +1,17 @@
+(ns binary-search)
+
+(defn middle
+  [lst]
+  (int (/ (count lst) 2)))
+
+(defn search-for 
+  [item lst]
+  (let [mid (middle lst)
+        curr (nth lst mid)]
+  (cond 
+    (= curr item) mid
+    (or (= mid (count lst)) (zero? mid)) (throw (Exception. (format "%s not found" item)))
+    (< curr item) (+ mid (search-for item (drop mid lst)))
+    (> curr item) (search-for item (take mid lst)))))
+
+
