1e2f864d87f3950f0504376f011c3bb52de53322
Zero Delta <zerodeltahedge@gmail.com>
2018-07-09 02:32:57 +0300

    meetup

---
+++ meetup/src/meetup.clj
@@ -0,0 +1,42 @@
+(ns meetup
+    (:import (java.util Calendar)))
+
+(def days-of-the-week {:monday 2 :tuesday 3 :wednesday 4 :thursday 5 :friday 6 :saturday 7 :sunday 1})
+
+(defn start-of-week
+  [month wk]
+  (case wk
+    :first 1
+    :second 8
+    :third 15
+    :fourth 22
+    :teenth 13
+    :last (if (= 2 month) 23 25)))
+
+(defn day-of-week
+  [year month week-start]
+  (let [cal (Calendar/getInstance)]
+    (.set cal Calendar/YEAR year)
+    (.set cal Calendar/MONTH (dec month))
+    (.set cal Calendar/DAY_OF_MONTH week-start)
+    (.get cal Calendar/DAY_OF_WEEK))) 
+
+(defn days-offset
+  "Calculates offset in days"
+  [from to]
+  (if (> from to)
+    (+ (- 7 from) to)
+    (- to from)))   
+
+(defn calculate-day
+  [year month week-start day-in-week]
+  (+ week-start (days-offset (day-of-week year month week-start) day-in-week)))
+    
+(defn meetup 
+  [month year day-in-week week] 
+  (vector year
+          month
+          (calculate-day year
+                         month
+                         (start-of-week month week)
+                         (days-of-the-week day-in-week))))
