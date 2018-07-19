f19d02815723ca6abfc4cd26524cfa8c6906f2f7
Zero Delta <zerodeltahedge@gmail.com>
2018-07-19 18:02:03 +0300

    triangle

---
+++ triangle/src/triangle.clj
@@ -0,0 +1,10 @@
+(ns triangle)
+
+(defn type 
+      [a b c] 
+      (let [sorted (sort > [a b c])]
+        (if (>= (first sorted) (apply + (rest sorted))) :illogical 
+            (case (count (set sorted))
+            1 :equilateral
+            2 :isosceles
+            3 :scalene))))
