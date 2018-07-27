6bd63c27afe33bffe6334faf4ee30ac8eaea6f64
Zero Delta <zerodeltahedge@gmail.com>
2018-07-28 02:09:58 +0300

    octal all tests passing

--- octal/src/octal.clj
+++ octal/src/octal.clj
@@ -1,5 +1,8 @@
 (ns octal)
 
-(defn to-decimal [] ;; <- arglist goes here
-  ;; your code goes here
-  )
+(defn to-decimal 
+    [oct-str] 
+    (if (re-matches #"[0-7]+" oct-str)
+      (apply + 
+        (map-indexed (fn [idx digit] (bit-shift-left (Character/getNumericValue digit) (* 3 idx))) (reverse oct-str)))
+       0))
