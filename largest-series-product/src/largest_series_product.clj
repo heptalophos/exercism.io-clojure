110f737379a53e368b97e1189ab7df25c657601a
Zero Delta <zerodeltahedge@gmail.com>
2018-09-05 19:54:30 +0300

    largest-series-product

---
+++ largest-series-product/src/largest_series_product.clj
@@ -0,0 +1,9 @@
+(ns largest-series-product)
+
+(defn largest-product 
+  [size digits]
+  (cond
+      (zero? size) 1 
+      (> size (count digits)) (throw (Exception. "span should be less than length of digits"))
+      :else (apply max (map (partial apply *) 
+            (partition size 1 (map (fn [x] {:pre [(Character/isDigit x)]} (Character/digit x 10)) digits))))))
