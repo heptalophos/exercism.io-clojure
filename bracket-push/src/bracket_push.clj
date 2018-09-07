b067e6c242ed187e2163191f73a5a5143d902d67
Zero Delta <zerodeltahedge@gmail.com>
2018-09-08 02:57:55 +0300

    refactor bracket-push, all tests passing

--- bracket-push/src/bracket_push.clj
+++ bracket-push/src/bracket_push.clj
@@ -1,4 +1,15 @@
 (ns bracket-push)
 
-(defn valid? [] 
-)
+(defn valid? [s]
+    (->> s
+        (filter #{\[ \] \( \) \{ \}})
+        (reduce
+            (fn [stack item]
+                (cond
+                    (#{ \( \{ \[ } item) (conj stack item)
+                    (and (#{ \( \{ \[ } (last stack))
+                         (= ({ \) \(, \} \{, \] \[ } item) (last stack)))
+                    (pop stack)
+                    :else (conj stack item)))
+        [])
+    empty?))
