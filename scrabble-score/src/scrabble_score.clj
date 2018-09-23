04311d629a054e6c2d43350f9e81623ab8d4c650
Zero Delta <zerodeltahedge@gmail.com>
2018-09-24 01:19:20 +0300

    scrabble-score refactor

--- scrabble-score/src/scrabble_score.clj
+++ scrabble-score/src/scrabble_score.clj
@@ -1,30 +1,26 @@
 (ns scrabble-score)
 
-(def scores { "AEIOULNRST" 1
-              "DG" 2
-              "BCMP" 3
-              "FHVWY" 4
-              "K" 5
-              "JX" 8
-              "QZ" 10
+(def values { 1 "AEIOULNRST"
+              2 "DG"
+              3 "BCMP"
+              4 "FHVWY"
+              5 "K"
+              8 "JX"
+              10 "QZ"
             })
 
-(defn- pairwise
-  [pairs letters score]
-  (conj pairs (reduce
-                (fn [a letter] (conj a [letter score]))
-                []
-                letters)))
-
-
-(def score
-  (into {} (apply concat (reduce-kv pairwise [] scores))))
-
+(def scores
+  (into {} (for [[value letters] values letter letters]
+                  [letter value])))
 
 (defn score-letter [letter]
-  (score (clojure.string/upper-case letter)))
+  (get scores letter 1))
 
+; (defn score-word [word]
+;   (reduce + (map scores (clojure.string/upper-case word))))
 
-(defn score-word  [word]
-  (reduce + 0 (map score-letter word)))
-
+(defn score-word [word]
+  (->> word
+       clojure.string/upper-case
+       (map scores)
+       (reduce +)))
