ce9e3f8b905589f209f92b4f8b314ce2fea38aeb
Zero Delta <zerodeltahedge@gmail.com>
2018-10-03 04:06:35 +0300

    pascals-triangle

---
+++ pascals-triangle/src/pascals_triangle.clj
@@ -0,0 +1,9 @@
+(ns pascals-triangle)
+
+(defn- subsequent [present]
+  (into [] (map + (cons 0 present) (conj present 0))))
+
+(def triangle (iterate subsequent [1N]))
+
+(defn row [n]
+  (last (take n triangle)))
