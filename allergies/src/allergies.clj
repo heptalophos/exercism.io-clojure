c5cd301f836bc35272e4f4ea69f8dde6796e33f4
Zero Delta <zerodeltahedge@gmail.com>
2018-10-30 23:57:47 +0200

    Update allergies/src/allergies.clj

--- allergies/src/allergies.clj
+++ allergies/src/allergies.clj
@@ -1,8 +1,14 @@
-(ns allergies)
+(ns allergies )
 
 
-(def allergens 
-  (zipmap [:eggs :peanuts :shellfish :strawberries :tomatoes :chocolate :pollen :cats] (range)))
+(def allergens {:eggs 1 
+                :peanuts 2 
+                :shellfish 4 
+                :strawberries 8
+                :tomatoes 16 
+                :chocolate 32 
+                :pollen 64 
+                :cats 128})
 
 
 (defn allergies [code]
