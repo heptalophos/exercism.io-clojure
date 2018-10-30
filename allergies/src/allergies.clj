98911b6c360daef7e048307e0d28543442870287
Zero Delta <zerodeltahedge@gmail.com>
2018-10-30 23:29:35 +0200

    allergies solution

--- allergies/src/allergies.clj
+++ allergies/src/allergies.clj
@@ -1,9 +1,16 @@
 (ns allergies)
 
-(defn allergies [] ;; <- arglist goes here
-  ;; your code goes here
-)
 
-(defn allergic-to? [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(def allergens 
+  (zipmap [:eggs :peanuts :shellfish :strawberries :tomatoes :chocolate :pollen :cats] (range)))
+
+
+(defn allergies [code]
+  (vec (for [[score allergen] allergens :when (pos? (bit-and code allergen))] score)))
+
+
+(defn allergic-to? [code allergen] 
+  (-> allergen
+      allergens
+      (bit-and code)
+      pos?))
