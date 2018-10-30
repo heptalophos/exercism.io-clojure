580b3093eab6edd23a2209581b3470d9c40fcda3
Zero Delta <zerodeltahedge@gmail.com>
2018-10-30 23:55:19 +0200

    Update allergies/project.clj

--- allergies/project.clj
+++ allergies/project.clj
@@ -1,4 +1,5 @@
 (defproject allergies "0.1.0-SNAPSHOT"
   :description "allergies exercise."
   :url "https://github.com/exercism/clojure/tree/master/exercises/allergies"
-  :dependencies [[org.clojure/clojure "1.9.0"]])
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/math.numeric-tower "0.0.2"]])
