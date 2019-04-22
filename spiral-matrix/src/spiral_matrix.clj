37e9de3dc60c5d1da4ad14de80dced3bb4bc2942
Zero Delta <zerodeltahedge@gmail.com>
2019-04-22 17:55:49 +0300

    spiral-matrix solution

--- spiral-matrix/src/spiral_matrix.clj
+++ spiral-matrix/src/spiral_matrix.clj
@@ -1,15 +1,16 @@
 (ns spiral-matrix)
 
-(defn spiral [n]
+(defn spiral 
+  [n]
   (if (pos? n)
-    (partition n 
-               (map inc 
-                    (map first 
-                         (sort-by second 
-                                  (map-indexed vector
-                                               (reductions + 
-                                                           (mapcat #(repeat %2 %1) (cycle [1 n -1 (- n)])
-                                                                   cons n 
-                                                                   (mapcat #(repeat 2 %) 
-                                                                           (range (dec n) 0 -1))))))))
-    '())))
+    (->> (range (dec n) 0 -1)
+         (mapcat #(repeat 2 %))
+         (cons n)
+         (mapcat #(repeat %2 %1) (cycle [1 n -1 (- n)]))
+         (reductions +)
+         (map-indexed vector)
+         (sort-by second)
+         (map first)
+         (map inc)
+         (partition n))
+    '()))
