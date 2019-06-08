ced70a661559595fd413e2b5328ba725211fc1c7
Zero Delta <zerodeltahedge@gmail.com>
2019-06-08 11:50:39 +0300

    rotational-cipher solution

---
+++ rotational-cipher/src/rotational_cipher.clj
@@ -0,0 +1,15 @@
+(ns rotational-cipher)
+
+
+(defn rotate [text key] 
+    (letfn [(rot26 [ch]
+        (cond
+            (Character/isLetter ch) ;; only rotates alpha characters
+                (let [base (cond (Character/isUpperCase ch) (int \A) 
+                                 :else (int \a))]
+                (char (+ base (mod (+ (- (int ch) base) key) 26))))
+            :else ch))]
+    (->> text
+         char-array
+         (map #(rot26 %))
+         (apply str)))) 
