673801774223399ed6c6af92bf2593e3b7a7b97a
Zero Delta <zerodeltahedge@gmail.com>
2019-11-20 14:21:58 +0200

    Reformat rna-transcription solution

--- rna-transcription/src/rna_transcription.clj
+++ rna-transcription/src/rna_transcription.clj
@@ -2,9 +2,11 @@
 
 (def dna->rna {\T "A", \A "U", \C "G", \G "C"})
 
-(defn dna? [sequence]
+(defn dna? 
+    [sequence]
     (re-matches #"[GCAT]+" sequence))
 
-(defn to-rna [sequence] 
+(defn to-rna 
+    [sequence] 
     (assert (dna? sequence))
     (apply str (map dna->rna sequence)))
