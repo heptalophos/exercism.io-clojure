956b94090fb40823cceb64cd93a4aa1b16fc2544
Zero Delta <zerodeltahedge@gmail.com>
2019-11-22 16:52:14 +0200

    Update armstrong-numbers README

--- armstrong-numbers/project.clj
+++ armstrong-numbers/project.clj
@@ -1,10 +1,4 @@
 (defproject armstrong-numbers "0.1.0-SNAPSHOT"
   :description "armstrong-numbers exercise"
   :url "https://github.com/exercism/clojure/tree/master/exercises/armstrong-numbers"
-  :dependencies [[org.clojure/clojure "1.8.0"]]
-  ; :main ^:skip-aot armstrong-numbers
-  ; :target-path "target/%s"
-  ; :profiles {:dev {:dependencies [[midje "1.7.0"]]}
-  ;                  :plugins [[lein-midje "3.2.1"]]         
-  ;                  :uberjar {:aot :all}}
-)
+  :dependencies [[org.clojure/clojure "1.10.0"]])
