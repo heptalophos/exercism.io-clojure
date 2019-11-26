af2e680410bae9330f0ac6cda0a320a3656af904
Zero Delta <zerodeltahedge@gmail.com>
2019-11-26 17:32:07 +0200

    Reformat clock.clj

--- clock/src/clock.clj
+++ clock/src/clock.clj
@@ -4,10 +4,12 @@
   (format "%02d:%02d" (:hour clock) (:minute clock)))
 
 (defn clock [hours minutes]
-  (let [time (mod (+ (* hours 60) minutes) (* 60 24))
+  (let [time (mod (+ (* hours 60) minutes) 
+                  (* 60 24))
         hrs (mod (quot time 60) 24)
         min (mod time 60)]
   {:hour hrs :minute min}))
 
 (defn add-time [clck time] 
-  (clock (:hour clck) (+ (:minute clck) time)))
+  (clock (:hour clck) 
+         (+ (:minute clck) time)))
