e8ac2a9231b30847a3b896d2341555b6a241fafd
Zero Delta <zerodeltahedge@gmail.com>
2019-11-28 02:36:52 +0200

    run-length-encoding refactor

--- run-length-encoding/src/run_length_encoding.clj
+++ run-length-encoding/src/run_length_encoding.clj
@@ -1,11 +1,24 @@
 (ns run-length-encoding)
 
-(defn pack-repeats 
-  "packs substrings of repeated characters into a <num><char> substring"
+(defn- pack-repeats 
+  "packs substrings of a repeated character into a 
+   substring consisting of the count of occurences
+   of the character followed by the character. 
+   Example: AAAAA -> 5A "
   [coll]
   (if (= (count coll) 1)
     (str (first coll))
     (str (count coll) (first coll))))
+
+(defn- unpack-repeats 
+  "unpacks a substring consisting of a number n and a character 
+   into a substring of the character repeated n times "
+  [s]
+  (if (Character/isDigit (first s))
+    (let [split (split-with #(Character/isDigit %) s)
+          num (Integer/parseInt (apply str (first split)))
+          chr (first (second split))]
+      (apply str (repeat num chr))) s))
 
 (defn run-length-encode
   "encodes a string with rle"
@@ -13,28 +26,11 @@
   (->> s 
        (partition-by identity)
        (map pack-repeats)
-       (apply str))
-)
-
-(defn unpack-repeats
-  [n c]
-  (apply str (repeat n c)) 
-)
-
-(defn unpack 
-  "unpacks strings like <num><char> into <num>-length sequences of <char>"
-  [s]
-  (if (Character/isDigit (first s))
-    (let [split (split-with #(Character/isDigit %) s)
-          num (Integer/parseInt (apply str (first split)))
-          ltr (first (second split))]
-      (unpack-repeats num ltr)) s)
-)
+       (apply str)))
 
 (defn run-length-decode
   "decodes an rle string"
   [s]
   (->> s (re-seq #"\d*\D{1}")
-         (map unpack)
-         (apply str))
-)
+         (map unpack-repeats)
+         (apply str)))
