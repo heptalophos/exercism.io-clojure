233840a442dfd0a6ba6cb8c833d9776bbdc25a15
Zero Delta <zerodeltahedge@gmail.com>
2020-03-28 16:09:45 +0200

    Update reverse_string.clj

--- reverse-string/src/reverse_string.clj
+++ reverse-string/src/reverse_string.clj
@@ -1,7 +1,12 @@
 (ns reverse-string
-  (:require [clojure.string :refer [join]]))
+  (:require [clojure.string 
+             :refer [join]]))
 
 (defn reverse-string
   "reverses a string" 
   [input]
-  (join (-> input str seq reverse)))
+  (join (-> 
+         input 
+         str 
+         seq 
+         reverse)))
