5a47e363c105b1140023ab31d349dee1a80c16f6
Zero Delta <zerodeltahedge@gmail.com>
2020-03-29 18:28:21 +0300

    Refactor updated triangle

--- triangle/src/triangle.clj
+++ triangle/src/triangle.clj
@@ -1,10 +1,26 @@
 (ns triangle)
 
-(defn type 
+(defn triangle-type 
       [a b c] 
       (let [sorted (sort > [a b c])]
-        (if (>= (first sorted) (apply + (rest sorted))) :illogical 
+        (if (>= (first sorted) 
+                (apply + (rest sorted))) 
+                :illogical 
             (case (count (set sorted))
             1 :equilateral
             2 :isosceles
             3 :scalene))))
+
+(defn equilateral?
+      [a b c]
+      (= (triangle-type a b c) :equilateral))
+
+(defn isosceles?
+      [a b c]
+      (or 
+        (= (triangle-type a b c) :equilateral)
+        (= (triangle-type a b c) :isosceles)))
+
+(defn scalene?
+      [a b c] 
+      (= (triangle-type a b c) :scalene))
