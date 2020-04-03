71925d4b8d4eac2928ddf2a5722a25c2afd984ab
Zero Delta <zerodeltahedge@gmail.com>
2020-04-03 14:39:27 +0300

    Update rotational_cipher.clj

--- rotational-cipher/src/rotational_cipher.clj
+++ rotational-cipher/src/rotational_cipher.clj
@@ -4,11 +4,18 @@
 (defn rotate [text key] 
     (letfn [(rot26 [ch]
         (cond
-            (Character/isLetter ch) ;; only rotates alpha characters
-                (let [base (cond (Character/isUpperCase ch) (int \A) 
-                                 :else (int \a))]
-                (char (+ base (mod (+ (- (int ch) base) key) 26))))
-            :else ch))]
+            (Character/isLetter ch) 
+            ;; only rotates alpha characters
+                (let [base 
+                        (cond (Character/isUpperCase ch) 
+                                (int \A) 
+                              :else 
+                                (int \a))]
+                (char (+ base 
+                         (mod (+ (- (int ch) base) 
+                                 key) 26))))
+            :else 
+                ch))]
     (->> text
          char-array
          (map #(rot26 %))
