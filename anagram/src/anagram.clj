26d002f6d50888c8f46a3ddfbf30d860febcc56f
Zero Delta <zerodeltahedge@gmail.com>
2020-12-07 16:16:56 +0200

    Refactor anagram.clj

--- anagram/src/anagram.clj
+++ anagram/src/anagram.clj
@@ -5,19 +5,19 @@
                      upper-case]]))
 
 (defn- string-sort 
-  "Sort a string"
+  "Sort a string, not case sensitive"
   [w]
   (join "" (map str (sort (seq (lower-case w)))))
 )
 
 (defn- anagram? 
-  "compute if candidate is anagram of word"
+  "is candidate an anagram of word"
   [word cand]
   (= word (string-sort cand))
 )
 
 (defn- not-same? 
-  "compute if candidate is not the same as the word"
+  "candidate is not the same as the word"
   [word cand]
   (not= word cand)  
 )  
@@ -29,11 +29,11 @@
 )  
 
 (defn anagrams-for
-  "words list from words that are anagram with anagram"
-  [anagram word-list]
-  (let [sorted-anagram (string-sort anagram)]
+  "returns a list of anagrams of word from word-list"
+  [word word-list]
+  (let [sorted-anagram (string-sort word)]
   (filter 
     #(and (anagram? sorted-anagram %)
-          (not-same? anagram %)
-          (not-capital? anagram %))
+          (not-same? word %)
+          (not-capital? word %))
     word-list) ))
