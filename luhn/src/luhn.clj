5862f999a671c501e9f47624cbbf5181fb490371
Zero Delta <zerodeltahedge@gmail.com>
2020-12-16 01:21:23 +0200

    Update luhn.clj

--- luhn/src/luhn.clj
+++ luhn/src/luhn.clj
@@ -1,10 +1,15 @@
-(ns luhn)
+(ns luhn
+  (:require [clojure.string :refer [replace]]))
 
 (defn valid? [n]
-  (if-let [normal (re-matches #"^\d\d+$" (clojure.string/replace n #" " ""))]
-    (->> (map #(Character/digit % 10) (remove #{\space} (str normal)))
+  (if-let [normal (re-matches #"^\d\d+$" 
+                      (replace n #" " ""))]
+    (->> (map #(Character/digit % 10) 
+               (remove #{\space} (str normal)))
         (reverse)
-        (map-indexed #(if (odd? %1) (* 2 %2) %2))
+        (map-indexed #(if (odd? %1) 
+                          (* 2 %2) 
+                          %2))
         (map #(if (> % 9) (- % 9) %))
         (apply +)
         (#(rem % 10))
