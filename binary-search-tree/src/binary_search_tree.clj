6c448dae560112794af3754b7e904e6aae62066f
Zero Delta <zerodeltahedge@gmail.com>
2021-05-07 19:34:54 +0300

    Reformat binary_search_tree.clj

--- binary-search-tree/src/binary_search_tree.clj
+++ binary-search-tree/src/binary_search_tree.clj
@@ -1,34 +1,45 @@
 (ns binary-search-tree)
 
-(defn value [node]
-  (:value node ))
+(defn value 
+      [node]
+      (:value node ))
 
-(defn left [node]
-  (:left node))
+(defn left 
+      [node]
+      (:left node))
 
-(defn right [node]
-  (:right node))
+(defn right 
+      [node]
+      (:right node))
 
-(defn singleton [value] 
-  {:value value nil nil} )
+(defn singleton 
+      [value] 
+      {:value value nil nil} )
 
-(defn insert [val node]
-  (cond (nil? node) 
-          (singleton val)
-        (<= val (value node)) 
-          (assoc node :left (insert val (left node)))
-        (> val (value node)) 
-          (assoc node :right (insert val (right node)))
-        :else 
-          node ))
+(defn insert 
+      [val node]
+      (cond (nil? node) 
+              (singleton val)
+            (<= val (value node)) 
+              (assoc node :left 
+                    (insert val (left node)))
+            (> val (value node)) 
+              (assoc node :right 
+                    (insert val (right node)))
+            :else 
+              node ))
 
-(defn to-list [tree]
-  (if (nil? tree)
-    []
-    (concat 
-     (to-list (left tree)) 
-     [(value tree)] 
-     (to-list (right tree)) ) ))
+(defn to-list 
+      [tree]
+      (if (nil? tree)
+          []
+          (concat 
+            (to-list (left tree)) 
+            [(value tree)] 
+            (to-list (right tree)) ) ))
 
-(defn from-list [values] 
-  (reduce (fn [acc val] (insert val acc)) nil values))
+(defn from-list 
+      [values] 
+      (reduce (fn [acc val] (insert val acc)) 
+              nil 
+              values))
