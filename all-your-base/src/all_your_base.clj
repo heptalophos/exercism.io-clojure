1dd7a6a24372b6c7cfc3b1b24003d32db76c2d27
Chris <zerodeltahedge@gmail.com>
2021-09-05 01:27:57 +0300

    Update all_your_base.clj

--- all-your-base/src/all_your_base.clj
+++ all-your-base/src/all_your_base.clj
@@ -2,23 +2,23 @@
 
 (defn- digs->dec [digits base]
     (loop [n 0 d digits]
-      (if (empty? d) 
-     	  n 
-		  (recur (+ (first d) (* base n)) (rest d)))))
+       (if (empty? d) 
+         n 
+         (recur (+ (first d) (* base n)) (rest d)))))
 
 (defn- dec->digs [num base]
     (loop [ds [] cur num]
-        (if (zero? cur) 
-            (if (empty? ds ) '(0) ds)
-            (recur (cons (mod cur base) ds)
-                   (quot cur base)))))
+       (if (zero? cur) 
+          (if (empty? ds ) '(0) ds)
+          (recur (cons (mod cur base) ds)
+                 (quot cur base)))))
 
 (defn convert [inbase digits outbase] 
 	(if (or (< inbase 2) 
-			(< outbase 2) 
-			(not (empty? (filter neg? digits)))
-			(not (empty? (filter #(>= % inbase) digits)))) 
+		(< outbase 2) 
+		(not (empty? (filter neg? digits)))
+		(not (empty? (filter #(>= % inbase) digits)))) 
 		nil
 		(if (empty? digits) 
-			'()
-			(dec->digs (digs->dec digits inbase) outbase))))
+		  '()
+		  (dec->digs (digs->dec digits inbase) outbase))))
