b1561f59377a99a21e392b1e5ddbbc0e331a5c47
Zero Delta <zerodeltahedge@gmail.com>
2021-09-05 20:50:17 +0300

    Update hexadecimal.clj

--- hexadecimal/src/hexadecimal.clj
+++ hexadecimal/src/hexadecimal.clj
@@ -4,12 +4,19 @@
 
 (def powers (iterate #(* % 16) 1))
 
-(defn hex-to-int [hex]
-  (if (every? digits hex) 
-      (->> hex
-           (map digits)
-           reverse
-           (map * powers)
-           (reduce +))
-      0))
+(defn hex-to-int 
+     [hex]
+     "Converts a hexadecimal string to a decimal integer
+      by: 1. checking hexadecimal well-formedness, 
+          2. mapping hex digits to decimal values, 
+          3. reversing their order, 
+          4. raising them to powers of 16 and 
+          5. adding them up."
+     (if (every? digits hex) 
+          (->> hex
+               (map digits)
+               reverse
+               (map * powers)
+               (reduce +))
+          0))
 
