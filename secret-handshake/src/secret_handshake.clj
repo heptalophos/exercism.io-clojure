bc9256de4000dc828247b1814e2804cadbbfbb97
Zero Delta <zerodeltahedge@gmail.com>
2021-09-07 21:29:11 +0300

    Reformat secret-handshake

--- secret-handshake/src/secret_handshake.clj
+++ secret-handshake/src/secret_handshake.clj
@@ -1,9 +1,15 @@
 (ns secret-handshake)
 
-(defn commands [n] 
-    (->> [(if (= 2r00001 (bit-and n 0x01)) "wink")
-          (if (= 2r00010 (bit-and n 0x02)) "double blink")
-          (if (= 2r00100 (bit-and n 0x04)) "close your eyes")
-          (if (= 2r01000 (bit-and n 0x08)) "jump")]
-          ((if (= 16 (bit-and n 0x10)) reverse identity))
-          (filter identity)))
+(defn commands 
+    [n] 
+    (->> [(if (= 2r00001 (bit-and n 0x01)) 
+              "wink")
+          (if (= 2r00010 (bit-and n 0x02)) 
+              "double blink")
+          (if (= 2r00100 (bit-and n 0x04)) 
+              "close your eyes")
+          (if (= 2r01000 (bit-and n 0x08)) 
+              "jump")]
+         ((if (= 16 (bit-and n 0x10)) 
+              reverse identity))
+         (filter identity)))
