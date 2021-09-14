f7f8fbf7dc628c2a8fedcfe7359e17dff007b7fb
Zero Delta <zerodeltahedge@gmail.com>
2021-09-14 21:14:00 +0300

    Refactor pig_latin.clj

--- pig-latin/src/pig_latin.clj
+++ pig-latin/src/pig_latin.clj
@@ -1,5 +1,6 @@
 (ns pig-latin 
-    (:require [clojure.string :as s]))
+    (:require [clojure.string 
+               :refer [join replace split]]))
 
 (def pattern-vowel #"([aeiou]|xr|yt).+")
 (def pattern-conso #"(s?qu|[^aeiou]+)(.+)")
@@ -10,11 +11,11 @@
     (re-matches pattern-vowel word) 
         (str word "ay")
     (re-matches pattern-conso  word) 
-        (s/replace word pattern-conso "$2$1ay")
+        (replace word pattern-conso "$2$1ay")
     :else ""))
 
 (defn translate 
     [phrase]
-    (->> (s/split phrase #"\s+")
+    (->> (split phrase #"\s+")
          (map pig-latinize)
-         (s/join " ")))
+         (join " ")))
