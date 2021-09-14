1d09282598218c9c88a975d867328123825d1df7
Zero Delta <zerodeltahedge@gmail.com>
2021-09-14 21:09:20 +0300

    Update pig_latin.clj

--- pig-latin/src/pig_latin.clj
+++ pig-latin/src/pig_latin.clj
@@ -1,19 +1,20 @@
-(ns pig-latin)
+(ns pig-latin 
+    (:require [clojure.string :as s]))
 
-(def pattern-v #"([aeiou]|xr|yt).+")
-(def pattern-c #"(s?qu|[^aeiou]+)(.+)")
+(def pattern-vowel #"([aeiou]|xr|yt).+")
+(def pattern-conso #"(s?qu|[^aeiou]+)(.+)")
 
 (defn pig-latinize 
     [word]
-    (cond 
-        (re-matches pattern-v word) 
-            (str word "ay")
-        (re-matches pattern-c  word) 
-            (clojure.string/replace word pattern-c "$2$1ay")
-        :else ""))
+(cond 
+    (re-matches pattern-vowel word) 
+        (str word "ay")
+    (re-matches pattern-conso  word) 
+        (s/replace word pattern-conso "$2$1ay")
+    :else ""))
 
 (defn translate 
     [phrase]
-    (->> (clojure.string/split phrase #"\s+")
+    (->> (s/split phrase #"\s+")
          (map pig-latinize)
-         (clojure.string/join " ")))
+         (s/join " ")))
