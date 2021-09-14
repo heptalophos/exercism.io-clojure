e0bf5f9c35cb79d60cf48b2f9f0d1a5e2f79a40f
Zero Delta <zerodeltahedge@gmail.com>
2021-09-14 21:22:42 +0300

    Refactor rna-transcription

--- rna-transcription/src/rna_transcription.clj
+++ rna-transcription/src/rna_transcription.clj
@@ -1,12 +1,18 @@
 (ns rna-transcription)
 
+(def dna-pattern #"[GCAT]+")
+
 (def dna->rna {\T "A", \A "U", \C "G", \G "C"})
 
-(defn dna? 
+(defn dna?
+    "Checks a sequence of nucleotides
+     against the dna pattern to determine match"
     [sequence]
-    (re-matches #"[GCAT]+" sequence))
+    (re-matches dna-pattern sequence))
 
-(defn to-rna 
+(defn to-rna
+    "Checks if a sequence is actually dna
+     and then maps into the corresponding rna"
     [sequence] 
     (assert (dna? sequence))
     (apply str (map dna->rna sequence)))
