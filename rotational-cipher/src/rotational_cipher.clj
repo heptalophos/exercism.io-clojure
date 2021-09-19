38018d00b0a0524ac5007cefd0d7f3fa4edbd7af
Zero Delta <zerodeltahedge@gmail.com>
2021-09-20 00:23:17 +0300

    Reformat rotational_cipher.clj

--- rotational-cipher/src/rotational_cipher.clj
+++ rotational-cipher/src/rotational_cipher.clj
@@ -3,17 +3,13 @@
 
 (defn rotate [text key] 
     (letfn [(rot26 [ch]
-        (cond
-            (Character/isLetter ch) 
-            ;; only rotates alpha characters
-                (let [base 
-                      (cond (Character/isUpperCase ch) 
+            (cond (Character/isLetter ch) 
+               ;; only rotates alpha characters
+                (let [base (cond (Character/isUpperCase ch) 
                                 (int \A) 
                             :else 
                                 (int \a))]
-                (char (+ base 
-                         (mod (+ (- (int ch) base) 
-                                 key) 26))))
+                (char (+ base (mod (+ (- (int ch) base) key) 26))))
             :else 
                 ch))]
     (->> text
