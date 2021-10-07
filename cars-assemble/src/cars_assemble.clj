f66e29c0b77a74a369eef388b624da5e08e354df
Zero Delta <zerodeltahedge@gmail.com>
2021-10-07 17:47:29 +0300

    Refactor cars-assemble

--- cars-assemble/src/cars_assemble.clj
+++ cars-assemble/src/cars_assemble.clj
@@ -6,13 +6,14 @@
   "Returns the assembly line's production rate per hour,
    taking into account its success rate"
   [speed]
-  (let [success-rate (cond 
-                        (=  0 speed)    0.0
-                        (<= 1 speed 4)  1.0
-                        (<= 5 speed 8)  0.9
-                        (=  9 speed)    0.8
-                        (= 10 speed)    0.77)]
-   (* speed per-hour-baseline success-rate ))
+  (let [success-rate 
+          (condp >= speed 
+                 0  0.0
+                 4  1.0
+                 8  0.9
+                 9  0.8
+                 10 0.77)]
+  (* speed per-hour-baseline success-rate ))
 )
 
 (defn working-items
