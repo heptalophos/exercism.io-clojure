c653148659fb01c9520a81797b47177179bb291c
Zero Delta <zerodeltahedge@gmail.com>
2021-10-22 00:20:22 +0300

    Reformat allergies.clj

--- allergies/src/allergies.clj
+++ allergies/src/allergies.clj
@@ -1,22 +1,23 @@
 (ns allergies )
 
 
-(def allergens {:eggs 1 
-                :peanuts 2 
-                :shellfish 4 
-                :strawberries 8
-                :tomatoes 16 
-                :chocolate 32 
-                :pollen 64 
-                :cats 128})
+(def allergens {:eggs 1 :peanuts 2 :shellfish 4 
+                :strawberries 8 :tomatoes 16 
+                :chocolate 32 :pollen 64 :cats 128}
+)
 
 
 (defn allergies [code]
-  (vec (for [[score allergen] allergens :when (pos? (bit-and code allergen))] score)))
+  (vec (for [[score allergen] allergens 
+              :when 
+              (pos? (bit-and code allergen))] 
+    score))
+)
 
 
 (defn allergic-to? [code allergen] 
   (-> allergen
       allergens
       (bit-and code)
-      pos?))
+      pos?)
+)
