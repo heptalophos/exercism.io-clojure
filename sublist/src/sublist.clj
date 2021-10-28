4003c442b6efbedd5057da08af2f36d5a4f1b238
Zero Delta <zerodeltahedge@gmail.com>
2021-10-28 13:05:19 +0300

    Solve sublist

--- sublist/src/sublist.clj
+++ sublist/src/sublist.clj
@@ -1,5 +1,14 @@
-(ns sublist)
+(ns sublist
+    (:require [clojure.string 
+               :refer [join includes?]]))
 
-(defn classify [list1 list2] ;; <- arglist goes here
-      ;; your code goes here
+(defn classify 
+      [list1 list2] 
+      (let [s1 (join "," list1)
+            s2 (join "," list2)]
+            (cond 
+                  (= s1 s2)         :equal 
+                  (includes? s1 s2) :superlist
+                  (includes? s2 s1) :sublist
+                  :else             :unequal))
 )
