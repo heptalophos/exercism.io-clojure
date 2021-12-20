8e397498765f4cd0cfb66d63ccdef17bb818847c
Zero Delta <zerodeltahedge@gmail.com>
2021-12-20 18:04:09 +0200

    Update project.clj

--- change/project.clj
+++ change/project.clj
@@ -3,4 +3,5 @@
   :url "https://github.com/exercism/clojure/tree/master/exercises/change"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.0"]])
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/math.combinatorics "0.1.6"]])
