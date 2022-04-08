b74a7217ce7b2851b75d8c8d7340de26598a9645
Zero Delta <zerodeltahedge@gmail.com>
2022-04-08 23:13:59 +0300

    Update spiral_matrix.clj

--- spiral-matrix/src/spiral_matrix.clj
+++ spiral-matrix/src/spiral_matrix.clj
@@ -1,16 +1,5 @@
 (ns spiral-matrix)
 
-(defn spiral 
-  [n]
-  (if (pos? n)
-    (->> (range (dec n) 0 -1)
-         (mapcat #(repeat 2 %))
-         (cons n)
-         (mapcat #(repeat %2 %1) (cycle [1 n -1 (- n)]))
-         (reductions +)
-         (map-indexed vector)
-         (sort-by second)
-         (map first)
-         (map inc)
-         (partition n))
-    '()))
+(defn spiral [n] ;; <- arglist goes here
+  ;; your code goes here
+)
