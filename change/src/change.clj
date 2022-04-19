9b21888d76c4a357ca1ded60b307fda736fdc069
Zero Delta <zerodeltahedge@gmail.com>
2022-04-19 23:50:16 +0300

    Update change.clj

--- change/src/change.clj
+++ change/src/change.clj
@@ -1,5 +1,17 @@
 (ns change)
 
-(defn issue [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn issue 
+      [value coins]
+      {:pre [(integer? value) (set? coins)]}
+      (let  [coins (into (sorted-set) (filter #(<= % value) coins))]
+      (cond (zero? value) '()
+            (or (neg? value) 
+                (empty? coins)) (throw (IllegalArgumentException. "cannot change"))
+            :else
+            (loop [change []
+                   amount-left value]
+                  (if-let [high-coin (first (filter #(<= % amount-left) 
+                                            (sort > (vec coins))))]
+                        (recur (conj change high-coin)
+                               (- amount-left high-coin))
+                  (sort < change))))))
