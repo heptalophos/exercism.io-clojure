b51cb813cf07a2052c276742b38c416b44a10683
Zero Delta <zerodeltahedge@gmail.com>
2022-09-09 02:27:08 +0300

    Refactor dominoes

--- dominoes/src/dominoes.clj
+++ dominoes/src/dominoes.clj
@@ -1,5 +1,24 @@
 (ns dominoes)
 
-(defn can-chain? [] ;; <- arglist goes here
-  ;; your code goes here
-)
+
+(defn- remaining 
+    [d ds]
+    (for [i (range (count ds))
+         :let [[s [[a b] & r]] (split-at i ds)
+               m (cond (= a d) b (= b d) a)]
+         :when m]
+         [m (concat s r)]))
+
+(defn- boundary 
+    [d ds]
+    (some (fn [[d ds]]
+            (if (empty? ds ) 
+                d 
+                (boundary d ds)))
+          (remaining d ds)))
+
+(defn can-chain? 
+    [[[a b] & ds]] 
+    (cond (nil? a) true
+          (nil? ds) (= a b)
+          :else (= a (boundary b (vec ds)))))
