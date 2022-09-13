d399fcd0a11831bf54fb6d790a77345278489e32
Zero Delta <zerodeltahedge@gmail.com>
2022-09-13 20:05:34 +0300

    Update annalyns_infiltration.clj

--- annalyns-infiltration/src/annalyns_infiltration.clj
+++ annalyns-infiltration/src/annalyns_infiltration.clj
@@ -3,35 +3,30 @@
 (defn can-fast-attack?
   "Returns true if a fast-attack can be made, false otherwise."
   [knight-awake?]
-  (if knight-awake? false true)
-)
+  (if knight-awake? false true))
 
 (defn can-spy?
-  "Returns true if the kidnappers can be spied upon, false otherwise."
-  [knight-awake? archer-awake? prisoner-awake?]
-  (cond
-    knight-awake? true
-    archer-awake? true
-    :else prisoner-awake?
-  )
-)
+    "Returns true if the kidnappers can be spied upon, false otherwise."
+    [knight-awake? archer-awake? prisoner-awake?]
+    (cond
+        knight-awake? true
+        archer-awake? true
+        :else prisoner-awake?))
 
 (defn can-signal-prisoner?
-  "Returns true if the prisoner can be signalled, false otherwise."
-  [archer-awake? prisoner-awake?]
-  (let [archer-sleeping (not archer-awake?)]
-    (if prisoner-awake? 
-      archer-sleeping
-      false)
-  )
-)
+    "Returns true if the prisoner can be signalled, false otherwise."
+    [archer-awake? prisoner-awake?]
+    (let [archer-sleeping (not archer-awake?)]
+        (if prisoner-awake? 
+            archer-sleeping
+            false)))
 
 (defn can-free-prisoner?
-  "Returns true if prisoner can be freed, false otherwise."
-  [knight-awake? archer-awake? prisoner-awake? dog-present?]
-  (let [archer-sleeping (not archer-awake?)
-        knight-sleeping (not knight-awake?)] 
-      (and archer-sleeping 
-           (or dog-present? (and prisoner-awake? 
-                             knight-sleeping))))
-)
+    "Returns true if prisoner can be freed, false otherwise."
+    [knight-awake? archer-awake? prisoner-awake? dog-present?]
+    (let [archer-sleeping (not archer-awake?) 
+          knight-sleeping (not knight-awake?)] 
+        (and archer-sleeping 
+            (or dog-present? 
+                (and prisoner-awake? 
+                     knight-sleeping)))))
