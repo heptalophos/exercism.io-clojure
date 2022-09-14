dd8d38ab8fe75523816fc41d370e09e7226a0e5b
Zero Delta <zerodeltahedge@gmail.com>
2022-09-14 23:51:03 +0300

    Update collatz_conjecture.clj

--- collatz-conjecture/src/collatz_conjecture.clj
+++ collatz-conjecture/src/collatz_conjecture.clj
@@ -4,10 +4,10 @@
   [n] 
   (letfn [(collatz ;; recurrence 
             [n]
-            (cond (= n 1) 1
+            (cond 
+                  (= n 1) 1
                   (even? n) (/ n 2)
-                  :else     (inc (* n 3))))]
+                  :else (inc (* n 3))))]
   (if (> n 0) 
     (count (take-while #(not= 1 %) (iterate collatz n)))
-    (throw 
-      (IllegalArgumentException. "number must be positive")))))
+    (throw (IllegalArgumentException. "number must be positive")))))
