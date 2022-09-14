aced7c37b8819031e774a7c2e0864447deb4b00d
Zero Delta <zerodeltahedge@gmail.com>
2022-09-14 23:50:14 +0300

    Update atbash_cipher.clj

--- atbash-cipher/src/atbash_cipher.clj
+++ atbash-cipher/src/atbash_cipher.clj
@@ -1,23 +1,16 @@
 (ns atbash-cipher
-  (:require [clojure.string 
-             :refer [join 
-                     lower-case 
-                     replace]]))
+  (:require [clojure.string :refer [join lower-case replace]]))
 
 (def encipher 
   (let [alpha "abcdefghijklmnopqrstuvwxyz"
         numeric "0123456789"]
-        (merge
-        (zipmap numeric numeric)
-        (zipmap alpha (reverse alpha)))))
-                        
+        (merge (zipmap numeric numeric)
+               (zipmap alpha (reverse alpha)))))         
 
 (defn encode 
     [plain] 
     (let [chunk-size 5]
-      (->> (-> plain 
-               lower-case
-               (replace #"[^a-z0-9]" ""))
+      (->> (-> plain lower-case (replace #"[^a-z0-9]" ""))
            (map encipher)
            (filter identity)
            (partition-all chunk-size)
