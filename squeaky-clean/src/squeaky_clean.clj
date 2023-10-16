891083ea7b4cdf7adba1c4baaa49d7a44c8e0111
Zero Delta <zerodeltahedge@gmail.com>
2023-10-17 00:39:02 +0300

    Refactor squeaky_clean.clj

--- squeaky-clean/src/squeaky_clean.clj
+++ squeaky-clean/src/squeaky_clean.clj
@@ -1,52 +1,37 @@
-(ns squeaky-clean
-  (:require [clojure.string :refer [split]]))
+(ns squeaky-clean (:require [clojure.string :refer [split]]))
 
-(defn- greek->nil
-  [s]
+(defn- greek->nil [s]
   (apply str (filter (fn [x] (not (nil? x)))
-                     (map #(if (and (<= 0x3B1 (int %))
-                                    (<= (int %) 0x3C9)) 
-                             nil 
-                             %)
-                          (seq s)))))
-(defn- ctrl-char->ctrl-str
-  [s]
+                     (map #(cond (and (<= 0x3B1 (int %)) (<= (int %) 0x3C9)) 
+                              nil :else %)
+                      (seq s)))))
+
+(defn- non-alpha->nil [s]
   (apply str (filter (fn [x] (not (nil? x)))
-                     (map #(if (Character/isISOControl %) 
-                             "CTRL" 
-                             %)
-                          (seq s)))))
-(defn- non-alpha->nil
-  [s]
-  (apply str (filter (fn [x] (not (nil? x)))
-                     (map #(if (or (Character/isAlphabetic (int %))
-                                   (= \_ %)) 
-                             % 
-                             nil)
-                          (seq s)))))
-(defn- space->underscore
-  [s]
-  (apply str (filter (fn [x] (not (nil? x)))
-                     (map #(if (= % \space) 
-                             \_ 
-                             %)
-                          (seq s)))))
-(defn- kebab->camel
-  [s]
+                     (map #(cond (or (Character/isAlphabetic (int %)) (= \_ %))
+                              %  :else nil)
+                      (seq s)))))
+
+(defn- ctrl-char->ctrl-str [s]
+  (apply str (map #(cond (Character/isISOControl %) "CTRL" :else %) (seq s))))
+
+(defn- space->underscore [s]
+  (apply str (map #(cond (Character/isWhitespace %) \_ :else %) (seq s))))
+
+(defn- kebab->camel [s]
   (let [[head & rest] (split s #"-")
-        capitalize (fn [x] (str (Character/toUpperCase (first x)) 
-                                (subs x 1)))]
-    (str head (apply str (map #(capitalize %) rest)))))
+        capitalize (fn [x] (str (Character/toUpperCase (first x)) (subs x 1)))]
+  (str head (apply str (map #(capitalize %) rest)))))
 
 (defn clean
-  "Cleans input string `s` of non-alpha and lower greek-alphabet letters,
-   converts space characters to underscores, ISOControl characters to a 
-  \"CTRL\" string and translates the string from kebab-case to camel-case."
+  "Cleans input string `s` of non-alpha and lower greek-alpha characters.
+   Converts space characters to underscores. 
+   Converts any ISOControl character to a \"CTRL\" string. 
+   Translates the string from kebab-case to CamelCase."
   [s]
   {:pre [(string? s)]}
-  (->> s
-       space->underscore 
-       ctrl-char->ctrl-str
-       kebab->camel
-       non-alpha->nil 
-       greek->nil))
+  (non-alpha->nil 
+   (greek->nil 
+    (space->underscore 
+     (ctrl-char->ctrl-str 
+      (kebab->camel s))))))
