38ab7f37beb8d977db6a5d8f8abda1974a80deea
Zero Delta <zerodeltahedge@gmail.com>
2023-10-24 15:33:23 +0300

    Refactor all-your-base

--- all-your-base/src/all_your_base.clj
+++ all-your-base/src/all_your_base.clj
@@ -1,26 +1,26 @@
 (ns all-your-base)
 
-(defn- digs->dec [digits base]
-    (loop [n 0 d digits]
-      	(if (empty? d) 
-     	  	n 
-		  	(recur (+ (first d) (* base n)) 
-				   (rest d)))))
+(defn- digs->dec 
+  [digits base]
+  (loop [n 0 d digits]
+      (if (empty? d)
+        n
+        (recur (+ (first d) (* base n)) (rest d)))))
 
-(defn- dec->digs [num base]
-    (loop [ds [] cur num]
-        (if (zero? cur) 
-            (if (empty? ds ) '(0) ds)
-            (recur (cons (mod cur base) ds)
-                   (quot cur base)))))
+(defn- dec->digs 
+  [num base]
+  (loop [ds [] cur num]
+      (if (zero? cur)
+        (if (not (seq ds)) '(0) ds)
+        (recur (cons (mod cur base) ds) (quot cur base)))))
 
-(defn convert [inbase digits outbase] 
-	(if (or (< inbase 2) 
-			(< outbase 2) 
-			(not (empty? (filter neg? digits)))
-			(not (empty? (filter #(>= % inbase) digits)))) 
-			nil
-			(if (empty? digits) 
-				'()
-				(dec->digs (digs->dec digits inbase) 
-						   outbase))))
+(defn convert
+  [inbase digits outbase]
+  (if (or (< inbase 2)
+          (< outbase 2)
+          (seq (filter neg? digits))
+          (seq (filter #(>= % inbase) digits))) 
+	nil
+    (if (not (seq digits))
+      '()
+      (dec->digs (digs->dec digits inbase) outbase))))
