e15e9f092f95ae5b619fb31c840c2cc4ce397f16
Zero Delta <zerodeltahedge@gmail.com>
2023-10-25 02:53:42 +0300

    Reformat matching_brackets.clj

--- matching-brackets/src/matching_brackets.clj
+++ matching-brackets/src/matching_brackets.clj
@@ -2,18 +2,18 @@
 
 (defn valid? 
   [s] 
-  (->> s
-      (filter #{\[ \] \( \) \{ \}})
-      (reduce
+  (->>  s
+        (filter #{\[ \] \( \) \{ \}})
+        (reduce
           (fn [stack item]
-              (cond
-                  (#{ \( \{ \[ } item) 
-                    (conj stack item)
-                  (and (#{ \( \{ \[ } (last stack))
-                       (= ({ \) \(, \} \{, \] \[ } item) 
-                       (last stack)))
-                    (pop stack)
-                  :else 
-                    (conj stack item)))
+            (cond
+              (#{ \( \{ \[ } item) 
+                (conj stack item)
+              (and (#{ \( \{ \[ } (last stack))
+                   (= ({ \) \(, \} \{, \] \[ } item) 
+                      (last stack)))
+                (pop stack)
+              :else 
+                (conj stack item)))
           [])
-      empty?))
+        empty?))
