e7e80b56c1bff071ada9d46c9a5cde6d372a683b
Zero Delta <zerodeltahedge@gmail.com>
2023-11-06 13:32:18 +0200

    Refactor all-your-base

--- all-your-base/src/all_your_base.clj
+++ all-your-base/src/all_your_base.clj
@@ -4,23 +4,23 @@
   [digits base]
   (loop [n 0 d digits]
       (if (not (seq d))
-         n
-         (recur (+ (first d) (* base n)) (rest d)))))
+          n
+          (recur (+ (first d) (* base n)) (rest d)))))
 
 (defn- dec->digs 
   [num base]
-  (loop [ds [] cur num]
-      (if (zero? cur)
+  (loop [ds [] current num]
+      (if (zero? current)
          (if (not (seq ds)) '(0) ds)
-         (recur (cons (mod cur base) ds) (quot cur base)))))
+         (recur (cons (mod current base) ds) (quot current base)))))
 
 (defn convert
   [inbase digits outbase]
   (if (or (< inbase 2)
           (< outbase 2)
           (seq (filter neg? digits))
-          (seq (filter #(>= % inbase) digits))) 
-	 nil
-     (if (not (seq digits))
-        '()
-        (dec->digs (digs->dec digits inbase) outbase))))
+          (seq (filter (fn [_] (>= _ inbase)) digits)))
+      nil
+      (if (not (seq digits))
+          '()
+          (dec->digs (digs->dec digits inbase) outbase))))
