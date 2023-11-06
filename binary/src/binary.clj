7d8b75cc14cb8efcc50948ddefa27705b44454be
Zero Delta <zerodeltahedge@gmail.com>
2023-11-06 13:36:21 +0200

    Refactor binary

--- binary/src/binary.clj
+++ binary/src/binary.clj
@@ -1,10 +1,10 @@
 (ns binary)
 
+#_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
 (defn to-decimal 
     [input-string]
     (->> (reverse input-string)
-         (filter #(or (= \0 %1) (= \1 %1)))
-         (map-indexed #(if (= %2 \1) (Math/pow 2 %1) 0))
-         (reduce + 0)
-         (int)
-    ))
+         (filter (fn [_] (or (= _ \0) (= _ \1))))
+         (map-indexed (fn [x y] (if (= y \1) (Math/pow 2 x) 0)))
+         (apply + 0)
+         (int)))
