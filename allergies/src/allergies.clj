dd510b340763435e78976680e253722ed72228e7
Zero Delta <zerodeltahedge@gmail.com>
2023-11-07 23:37:53 +0200

    Update allergies.clj

--- allergies/src/allergies.clj
+++ allergies/src/allergies.clj
@@ -1,7 +1,9 @@
 (ns allergies )
 
-(def allergens {:eggs 1 :peanuts 2 :shellfish 4 :strawberries 8 
-                :tomatoes 16 :chocolate 32 :pollen 64 :cats 128})
+(def ^:private allergens {:eggs 1 :peanuts 2
+                          :shellfish 4 :strawberries 8
+                          :tomatoes 16 :chocolate 32
+                          :pollen 64 :cats 128})
 
 (defn allergies [code]
   (vec (for [[score allergen] allergens 
