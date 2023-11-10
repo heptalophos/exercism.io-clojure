abdbf877e3aceacb010e43370454bef807b5280a
Zero Delta <zerodeltahedge@gmail.com>
2023-11-10 22:02:49 +0200

    Refactor change

--- change/src/change.clj
+++ change/src/change.clj
@@ -1,32 +1,31 @@
 (ns change)
 
 (defn- change
-       [value coins changed]
-       (cond 
-            (nil? (first coins)) 
-                changed
-            :else 
-                (change (mod value (first coins)) 
-                        (rest coins) 
-                        (concat (repeat (quot value (first coins)) 
-                                        (first coins)) 
-                                changed))))
+    [value coins changed]
+    (cond
+        (nil? (first coins))
+           changed
+      :else
+        (change (mod value (first coins)) (rest coins)
+                (concat (repeat (quot value (first coins)) (first coins))
+                        changed))))
 
+#_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
 (defn issue
-      [value coins]
-      {:pre [(integer? value) (set? coins)]}
-      (let [coins (into (sorted-set) (filter #(<= % value) coins))]
-      (cond 
-        (zero? value) 
+    [value coins]
+    {:pre [(integer? value) (set? coins)]}
+    (let [coins (into (sorted-set) (filter (fn [_ ] (<= _ value)) coins))] 
+    (cond
+        (zero? value)
             '()
-        (or (empty? coins) (neg? value) (every? #(not= (/ value %) (quot value %)) coins)) 
+        (or (not (seq coins)) (neg? value)
+            (every? (fn [_] (not= (/ value _) (quot value _))) coins))
             (throw (IllegalArgumentException. "cannot change"))
-        :else 
-            (->>    coins
-                    (sort-by -)
-                    (iterate rest)
-                    (take-while first)
-                    (map (fn [d] (change value d [])))
-                    (sort-by count)
-                    (first)
-              ))))
+      :else
+        (->>  coins
+              (sort-by -)
+              (iterate rest)
+              (take-while first)
+              (map (fn [_] (change value _ [])))
+              (sort-by count)
+              (first)))))
