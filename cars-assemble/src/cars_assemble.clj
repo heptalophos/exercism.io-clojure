40824ae20e0bc55f626a42a7a49427d31b044c36
Zero Delta <zerodeltahedge@gmail.com>
2023-11-10 20:16:56 +0200

    Reformat cars_assemble.clj

--- cars-assemble/src/cars_assemble.clj
+++ cars-assemble/src/cars_assemble.clj
@@ -6,22 +6,17 @@
   "Returns the assembly line's production rate per hour,
    taking into account its success rate"
   [speed]
-  (let [success-rate 
-          (condp >= speed 
-                 0  0.0
-                 4  1.0
-                 8  0.9
-                 9  0.8
-                 10 0.77)
-        true-speed 
-          (* speed success-rate)]
-  (* per-hour-baseline true-speed))
-)
+  (let [success-rate (condp >= speed 0  0.0
+                                     4  1.0
+                                     8  0.9
+                                     9  0.8
+                                    10  0.77)
+        true-speed (* speed success-rate)]
+  (* per-hour-baseline true-speed)))
 
 (defn working-items
   "Calculates how many working cars are produced per minute"
   [speed]
   (-> (production-rate speed)
       (/ 60)
-      int)
-)
+      int))
