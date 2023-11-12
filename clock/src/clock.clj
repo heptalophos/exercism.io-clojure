3db8fa6f2ae85f2209693ea7fea07df9f6ea6e41
Zero Delta <zerodeltahedge@gmail.com>
2023-11-13 00:20:42 +0200

    Update clock.clj

--- clock/src/clock.clj
+++ clock/src/clock.clj
@@ -1,15 +1,13 @@
 (ns clock)
 
-(defn clock->string [clock] 
-  (format "%02d:%02d" (:hour clock) (:minute clock)))
-
 (defn clock [hours minutes]
-  (let [time (mod (+ (* hours 60) minutes) 
-                  (* 60 24))
+  (let [time (mod (+ (* hours 60) minutes) (* 60 24))
         hrs (mod (quot time 60) 24)
         min (mod time 60)]
   {:hour hrs :minute min}))
 
-(defn add-time [clck time] 
-  (clock (:hour clck) 
-         (+ (:minute clck) time)))
+(defn clock->string [_clock]
+  (format "%02d:%02d" (:hour _clock) (:minute _clock)))
+
+(defn add-time [_clock time] 
+  (clock (:hour _clock) (+ (:minute _clock) time)))
