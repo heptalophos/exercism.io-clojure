6f0c6ac7db0b765f375db1bf7da00f1a776746c5
Zero Delta <zerodeltahedge@gmail.com>
2023-11-14 02:37:47 +0200

    Refactor leap

--- leap/src/leap.clj
+++ leap/src/leap.clj
@@ -2,12 +2,10 @@
 
 (defn- divides?
   [divisor n]
-  (if (== 0 (mod n divisor)) 
-      true 
-      false))
+  (if (== 0 (mod n divisor)) true false))
 
 (defn leap-year? 
   [year] 
-  (if (divides? 100 year)
-      (divides? 400 year)
-      (divides? 4 year)))
+  (cond (divides? 100 year) 
+        (divides? 400 year)
+  :else (divides? 4 year)))
