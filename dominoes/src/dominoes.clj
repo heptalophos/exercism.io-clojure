cd12aae9d145bda29322ead2276291329c7216a3
Zero Delta <zerodeltahedge@gmail.com>
2023-11-28 00:42:31 +0200

    Update dominoes.clj

--- dominoes/src/dominoes.clj
+++ dominoes/src/dominoes.clj
@@ -4,17 +4,17 @@
     [stone ds]
     (for [i (range (count ds))
          :let [[s [[a b] & rest]] (split-at i ds)
-                more (cond (= a stone) b (= b stone) a)]
+               more (cond (= a stone) b (= b stone) a :else nil)]
          :when more]
          [more (concat s rest)]))
 
 (defn- boundary 
     [stone ds]
     (some (fn [[stone ds]]
-            (if (not (seq ds)) 
-               stone 
-               (boundary stone ds))) 
-          (remaining stone ds)))
+            (if (not (seq ds))
+                stone
+                (boundary stone ds))) 
+            (remaining stone ds)))
 
 (defn can-chain? 
     [[[a-stone b-stone] & dominoes]] 
