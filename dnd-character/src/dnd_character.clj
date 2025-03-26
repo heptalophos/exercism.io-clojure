cb921cfa9d93f98565b9a1a7fdebe20a1d7be0e7
Zero Delta <zerodeltahedge@gmail.com>
2025-03-26 21:36:17 +0200

    Create dnd_character.clj

---
+++ dnd-character/src/dnd_character.clj
@@ -0,0 +1,28 @@
+(ns dnd-character)
+
+(defn- roll 
+    "Rolls -once- a six-faced die"
+    []
+    (inc (rand-int 6)))
+
+(defn score-modifier
+    "Calculates a score's modifier"
+    [score]
+    (int (Math/floor (/ (- score 10) 2))))
+
+(defn rand-ability
+    "Generates a random ability"
+    []
+    (reduce + (rest (sort (repeatedly 4 roll)))))
+
+(defn rand-character
+    "Generates a random character"
+    []
+    (let [constitution (rand-ability)]
+        {:strength  (rand-ability)
+         :dexterity (rand-ability)
+         :charisma  (rand-ability)
+         :wisdom    (rand-ability)
+         :intelligence (rand-ability)
+         :constitution (rand-ability)
+         :hitpoints (+ (score-modifier constitution) 10)}))
