72435e9d56c0a7da197e379d4c1d512b798605c9
Zero Delta <zerodeltahedge@gmail.com>
2025-04-04 00:52:00 +0300

    Update sublist.clj

--- sublist/src/sublist.clj
+++ sublist/src/sublist.clj
@@ -1,13 +1,26 @@
-(ns sublist
-    (:require [clojure.string :refer [join includes?]]))
+(ns sublist)
+
+(defn- contains-head?
+    "Checking recursively equality of heads, returns true if 
+     a list contains another, smaller list at its front."
+    [[x & xs] [y & ys]]
+    (and (or (= x y) (nil? y))
+         (or (nil? ys) (contains-head? xs ys))))
+
+(defn- sublist?
+    "Returns true if first list is sublist of second."
+    [xs ys]
+    (let [[p & ps] xs
+          [q & qs] ys]
+        (or (empty? xs)
+            (and (=  p q) (contains-head? qs ps))
+            (and (seq qs) (sublist? xs qs)))))
 
 (defn classify 
-      [list1 list2] 
-      (let [s1 (join "," list1)
-            s2 (join "," list2)]
-            (cond 
-             (= s1 s2)         :equal 
-             (includes? s1 s2) :superlist
-             (includes? s2 s1) :sublist
-             :else             :unequal))
-)
+    "Determine the sublistness of first argument against the second."
+    [xs ys]
+    (cond
+        (and (sublist? xs ys) (sublist? ys xs)) :equal
+        (sublist? xs ys) :sublist
+        (sublist? ys xs) :superlist
+        :else :unequal))
