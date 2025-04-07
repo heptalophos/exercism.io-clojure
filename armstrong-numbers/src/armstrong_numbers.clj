7d2945a6a43e2f83b5618d885d4ddc7040a15998
Zero Delta <zerodeltahedge@gmail.com>
2025-04-07 17:03:10 +0300

    Update armstrong_numbers.clj

--- armstrong-numbers/src/armstrong_numbers.clj
+++ armstrong-numbers/src/armstrong_numbers.clj
@@ -1,19 +1,20 @@
 (ns armstrong-numbers)
 
 (defn- ** 
-  "Returns the (integer) nth-power of x"
-  [x n] 
-  (->> x (repeat n) 
+    "Returns the (integer) nth-power of x"
+    [x n] 
+    (->> x
+         (repeat n)
          (reduce *)))
 
-#_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
 (defn armstrong?
-  "Checks if num is an armstrong 
-   (or narcissistic) number"
-  [num]
-  (->> num (str) 
-           (seq) 
-           (map (fn [d] (Character/digit d 10)))
-           (map (fn [x] (** x (count (str num))))) 
-           (reduce +)
-           (= num)))
+    "Checks if num is an armstrong 
+     (or narcissistic) number"
+    [num]
+    (->> num
+         (str)
+         (seq)
+         (map #(Character/digit % 10))
+         (map #(** % (count (str num))))
+         (reduce +)
+         (= num)))
