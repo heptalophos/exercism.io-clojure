00b257c1c25938d9f2eef78b87934bb43c000bcb
Zero Delta <zerodeltahedge@gmail.com>
2025-08-08 21:36:44 +0300

    Update binary_search.clj

--- binary-search/src/binary_search.clj
+++ binary-search/src/binary_search.clj
@@ -1,19 +1,19 @@
 (ns binary-search)
 
 (defn middle
-  [list]
-  (int (bit-shift-right (count list) 1)))
+  [lst]
+  (int (/ (count lst) 2)))
 
 (defn search-for 
-  [item list]
-  (let [mid (middle list)
-        current (nth list mid)]
-  (cond
-      (= current item)
-          mid
-      (or (= mid (count list)) (zero? mid))
-          (throw (Exception. (format "%s not found" item)))
-      (< current item)
-          (+ mid (search-for item (drop mid list)))
-      (> current item)
-          (search-for item (take mid list)))))
+  [item lst]
+  (let [mid (middle lst)
+        curr (nth lst mid)]
+  (cond 
+    (= curr item) mid
+    (or 
+      (= mid (count lst)) (zero? mid)) 
+        (throw (Exception. (format "%s not found" item))) ;; (- 0 1)
+      (< curr item) 
+        (+ mid (search-for item (drop mid lst)))
+      (> curr item) 
+        (search-for item (take mid lst)))))
