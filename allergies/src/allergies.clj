4f4bd2f0550bd5322a4512e631eda518aaac0e88
Zero Delta <zerodeltahedge@gmail.com>
2025-08-10 19:00:35 +0300

    Update allergies.clj

--- allergies/src/allergies.clj
+++ allergies/src/allergies.clj
@@ -11,7 +11,6 @@
   (vec (for [[allergen code] allergens :when (> (bit-and score code) 0)]
             allergen)))
 
-#_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
 (defn allergic-to? 
   [score allergen]
   (some (fn [_] (= _ allergen)) (allergies score)))
