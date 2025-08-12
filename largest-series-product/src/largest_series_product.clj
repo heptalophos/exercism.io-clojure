c95cec9412d130d1deccf356ac433e532a233a3a
Zero Delta <zerodeltahedge@gmail.com>
2025-08-12 23:21:49 +0300

    Reformat largest-series-product

--- largest-series-product/src/largest_series_product.clj
+++ largest-series-product/src/largest_series_product.clj
@@ -1,16 +1,16 @@
 (ns largest-series-product)
 
-(defn largest-product 
-  [size digits]
-  (cond
-    (zero? size) 
-      1 
-    (> size (count digits)) 
-      (throw (Exception. "span should be less than length of digits"))
-    :else 
-      (apply max (map (partial apply *)
-                      (partition size 1
-                          (map (fn [_] 
-                                 {:pre [(Character/isDigit _)]}
-                                 (Character/digit _ 10))
-                                 digits))))))
+(defn largest-product
+    [size digits]
+    (cond
+        (zero? size)
+            1
+        (> size (count digits))
+            (throw (Exception. "span should be less than length of digits"))
+        :else
+            (apply max (map (partial apply *)
+                            (partition size 1
+                                (map (fn [_]
+                                          {:pre [(Character/isDigit _)]}
+                                          (Character/digit _ 10))
+                                          digits))))))
